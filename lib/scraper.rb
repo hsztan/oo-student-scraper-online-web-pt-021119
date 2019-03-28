540f7bdc6022e2738b50534be1da73537b87664f
Henry Sztanski <henry@sztanski.com>
2019-03-28 23:37:06 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,15 +5,16 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    hash = {}
-    html = open('index_url')
-    doc = Nokogiri::HTML(html).css(".roster-class-containers")
-
-    name = doc.css(".student-name").text
-    location = doc.css(".student-location").text
-    profile = doc.ss("")
+    scraped_students = []
+    html = open(index_url)
+    doc = Nokogiri::HTML(html).css(".roster-cards-containers")
     binding.pry
 
+    # name = doc.css(".student-name").text
+    # location = doc.css(".student-location").text
+    #
+    # binding.pry
+    scraped_students
   end
 
   def self.scrape_profile_page(profile_url)
